d755e8ac97264b2f88c25c022643f55f8ad14545
Junior Camara <juniorcamara.1@hotmail.com>
2025-07-14 15:14:05 -0300

    Foi criado o código solicitando a entrada de dados e a exposição desses dados ao fim.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("BEM VINDO AO BANCO SANTANDER\n");
+        int numero;
+        String agencia, nomeCliente;
+        double saldo;
+        System.out.print("Digite o número da agência: ");
+        agencia = sc.nextLine();
+        System.out.print("Nome do cliente: ");
+        nomeCliente = sc.nextLine();
+        System.out.print("Número da conta: ");
+        numero = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Saldo: R$");
+        saldo = sc.nextDouble();
+        String mensagem = "Olá ".concat(nomeCliente)
+                .concat(", obrigado por criar uma conta em nosso banco, sua agência é ")
+                .concat(agencia).concat(", conta número ").concat(String.valueOf(numero)).concat(" e seu saldo de R$")
+                .concat(String.valueOf(saldo)).concat(" já está disponível para saque.");
+
+        System.out.println(mensagem);
+
+        sc.close();
     }
 }
